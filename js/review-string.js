4638e97d4556de47fd83818f56879fef316399ce
pravallika <pravallikaammineni@github.com>
2019-04-24 23:32:12 -0700

    reviewed string variable concat function

--- js/review-string.js
+++ js/review-string.js
@@ -9,3 +9,19 @@ var str2='World';
 
 console.log(str1);
 console.log(str2);
+
+var car= "Honda";//camel casing naming convention- main variables in project
+var model="Civic";//snake naming convention-properties inside object
+console.log(car,model);
+
+var carModel=car+'  '+model;
+console.log(carModel);
+
+var carModel=car+model;
+console.log(carModel);
+
+var carModel=car.concat(' ',model);
+console.log(carModel);
+
+var carModel=car.concat( ' ').concat(model);
+console.log(carModel);
