8abc3ff42e85470a84b487853d295780f17f1fc7
pravallika <pravallikaammineni@github.com>
2019-04-24 23:23:16 -0700

    reviewed string data types

---
+++ js/review-string.js
@@ -0,0 +1,11 @@
+var a= 'Hello world!';
+console.log(a);
+// c. output only the first word (i.e `Hello`)
+
+// d. output only the second word (i.e. `World`)
+
+var str1='Hello';
+var str2='World';
+
+console.log(str1);
+console.log(str2);
