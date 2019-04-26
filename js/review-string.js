4adb4431c963a16b5748d66e3d5632e328713f11
pravallika <pravallikaammineni@github.com>
2019-04-25 19:20:58 -0700

    changes in review-string

--- js/review-string.js
+++ js/review-string.js
@@ -17,12 +17,6 @@ console.log(car,model);
 var carModel=car+'  '+model;
 console.log(carModel);
 
-<<<<<<< HEAD
-var carModel=car+model;
-console.log(carModel);
-=======
-
->>>>>>> reviewed array variables
 
 var carModel=car.concat(' ',model);
 console.log(carModel);
